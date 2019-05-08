d4429927e550715e4aef3ab8e9fb569976bff869
hendrikpawlowski <hendrik_gregor.pawlowski@smail.th-koeln.de>
2019-05-08 16:34:28 +0200

    astronautenSchnittstelle implementiert

---
+++ src/raumstation/astronautenSchnittstelle/astronautenSchnittstelle.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+const hf = require('../../ourModules/helpFunctions')
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.question('\nIn welchem Raum möchten Sie die Temperatur anpassen?\n', (raum) => {
+  rl.question('\nUm wie viel Grad möchten Sie die Temperatur verändern?\n', (grad) => {
+
+    const message = {
+      versorgungsmittel: "temperatur",
+      raum: raum,
+      effekt: "change",
+      wert: grad
+    }
+
+    console.log("MESSAGE: " + JSON.stringify(message));
+
+    hf.produce('versorgungsroboter', 'versorgung', message);
+    rl.close();
+  })
+})
